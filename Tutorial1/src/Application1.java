2c4b347dad4fcde04c6597946b1c9c5e7068abf0
sjayashree <jshree@gmail.com>
2017-01-26 14:08:50 -0800

    Add Tutorial1

---
+++ Tutorial1/src/Application1.java
@@ -0,0 +1,25 @@
+
+public class Application1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+int myNumber = 100;
+short myshort = 376;
+long mylong = 453454;
+double mydouble = 10.45654645;
+char mychar = 'y';
+boolean mybool = true;
+byte mybyte = 127;
+
+
+System.out.println("myNumber is:" + myNumber);
+System.out.println("myshort is:" + myshort);
+System.out.println("mylong is:" + mylong);
+System.out.println("mydouble is:" + mydouble);
+System.out.println("mychar is:" + mychar);
+System.out.println("mybool is:" + mybool);
+System.out.println("mybyte is:" + mybyte);
+	}
+
+}
